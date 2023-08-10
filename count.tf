ac8dec4f480721868e50a569b645e69385fa0567
SiddheshC13 <140595930+SiddheshC13@users.noreply.github.com>
2023-08-10 13:43:59 +0530

    Update count.tf

--- count.tf
+++ count.tf
@@ -2,10 +2,7 @@ provider "azurerm" {
   features {
 
   }
-  subscription_id = "754a1399-426f-48cf-aa53-609abfb947db"
-  client_id       = "a199cec1-24a2-46d1-8ef4-9ca2630b8674"
-  client_secret   = "OBh8Q~5.xRLtkh4yHDDKUYcgqGfTEWnYlq63CaX3"
-  tenant_id       = "21824c05-eff6-477c-ba01-6443c7bba628"
+
 
 }
 
